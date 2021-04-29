13329185dfb14447e4e0b95b2c0faee1c9887a5b
swivan6 <sivan@uccs.edu>
2021-04-29 23:43:10 +0000

    Added devise auth helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    
+    user_signed_in?
+    
+    current_user
+    
+    user_session
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-    user_signed_in?
-    current_user
-    user_session
 end
